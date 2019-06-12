3cc0837c6fddc41735fee95226e01dfa3713b748
vagrant <vagrant@homestead>
2019-06-12 06:15:19 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Auth::routes(['verify' => True]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
