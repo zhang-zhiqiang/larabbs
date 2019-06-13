bee438f92656c9de3aeec40a722748dbac19e272
vagrant <vagrant@homestead>
2019-06-13 03:45:53 +0000

    翻译URL标识

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ Auth::routes(['verify' => True]);
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
