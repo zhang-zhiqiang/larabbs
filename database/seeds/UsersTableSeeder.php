a4ae59713f0ff96e68ec5fd4e5296af36370f829
vagrant <vagrant@homestead>
2019-06-14 03:15:50 +0000

    回复数据

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -44,8 +44,8 @@ class UsersTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = 'zzq1';
+        $user->email = '1205360416@qq.com';
         $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
     }
